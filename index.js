0532f11ddc5d6f904e5ffc11a9766e9467f4f276
Maximilian Rice <maximilian.rice1@gmail.com>
2020-10-27 20:03:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.error("HALP")
-console.log()
-console.warn()
+console.log("I would be a logger")
+console.warn("Flatiron should really do a better job at explaining things for their intro classes")
